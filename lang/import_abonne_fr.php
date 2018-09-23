fd8ee0d2b456a28abd415ee411a6315e175739f5
christophe le drean <christophe@rouge-rose.net>
2018-09-23 12:26:25 +0200

    Plugin d'import des abonnés

---
+++ lang/import_abonne_fr.php
@@ -0,0 +1,66 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_import_abonne' => 'Ajouter ce import',
+
+	// C
+	'champ_boite_postale_label' => 'Boite postale',
+	'champ_code_postal_label' => 'Code postal',
+	'champ_complement_label' => 'Complément',
+	'champ_duree_label' => 'Durée abonnement',
+	'champ_date_abonnement_label' => "Date d'abonnement",
+	'champ_email_label' => 'email',
+	'champ_id_abonnement_label' => 'id_abonnement',
+	'champ_id_auteur_label' => 'id_auteur',
+	'champ_montant_label' => 'Montant payé',
+	'champ_nom_label' => 'Nom',
+	'champ_numero_debut_label' => 'Numéro début',
+	'champ_numero_fin_label' => 'Numéro Fin',
+	'champ_organisation_label' => 'Organisation',
+	'champ_paiement_label' => 'Paiement',
+	'champ_pays_label' => 'Pays',
+	'champ_prenom_label' => 'Prénom',
+	'champ_region_label' => 'Région',
+	'champ_reabonnement_label' => 'Réabonnement',
+	'champ_nombre_abonnement_label' => "Nombre d'abonnement",
+	'champ_service_label' => 'Service',
+	'champ_type_abonnement_label' => 'Type abonnement',
+	'champ_ville_label' => 'Ville',
+	'champ_voie_label' => 'Voie',
+	'confirmer_supprimer_import_abonne' => 'Confirmez-vous la suppression de cet import ?',
+
+	// I
+	'icone_creer_import_abonne' => 'Créer un import',
+	'icone_modifier_import_abonne' => 'Modifier ce import',
+	'info_1_import_abonne' => 'Un import',
+	'info_aucun_import_abonne' => 'Aucun import',
+	'info_import_abonnes_auteur' => 'Les imports de cet auteur',
+	'info_nb_import_abonnes' => '@nb@ imports',
+
+	// R
+	'retirer_lien_import_abonne' => 'Retirer ce import',
+	'retirer_tous_liens_import_abonnes' => 'Retirer tous les imports',
+
+	// S
+	'supprimer_import_abonne' => 'Supprimer cet import',
+
+	// T
+	'texte_ajouter_import_abonne' => 'Ajouter un import',
+	'texte_changer_statut_import_abonne' => 'Ce import est :',
+	'texte_creer_associer_import_abonne' => 'Créer et associer un import',
+	'texte_definir_comme_traduction_import_abonne' => 'Ce import est une traduction du import numéro :',
+	'titre_import_abonne' => 'Import',
+	'titre_import_abonnes' => 'Imports',
+	'titre_import_abonnes_rubrique' => 'Imports de la rubrique',
+	'titre_langue_import_abonne' => 'Langue de ce import',
+	'titre_logo_import_abonne' => 'Logo de ce import',
+	'titre_objets_lies_import_abonne' => 'Liés à ce import',
+);
---
+++ lang/vimport_abonnes_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// V
+	'vimport_abonnes_titre' => 'Importer les abonnés',
+);
